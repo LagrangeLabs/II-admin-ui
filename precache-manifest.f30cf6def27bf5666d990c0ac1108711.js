2481ad817e72f0c25f75a5293f267896beca66c0
Travis <travis@travis-ci.com>
2020-07-02 08:37:16 +0000

    Updates

---
+++ precache-manifest.f30cf6def27bf5666d990c0ac1108711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195bd7a11d18891da1890bf1ab57de67",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d75650e41f8b9c1aac5d.bundle.js"
+  },
+  {
+    "url": "runtime~main.d75650e41f8b9c1aac5d.bundle.js"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.d75650e41f8b9c1aac5d.bundle.js"
+  },
+  {
+    "url": "vendors~main.d75650e41f8b9c1aac5d.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3c6fbb65087292e1f4543738f016a67f.js"
+  "precache-manifest.f30cf6def27bf5666d990c0ac1108711.js"
 );
 
 self.addEventListener('message', (event) => {
